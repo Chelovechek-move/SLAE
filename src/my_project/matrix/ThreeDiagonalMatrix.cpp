5a06c628f5c24547d34d9a753bafb304cc7abbe0
Chelovechek-move <81492323+Chelovechek-move@users.noreply.github.com>
2022-02-18 23:58:35 +0300

    Add files via upload

--- src/my_project/matrix/ThreeDiagonalMatrix.cpp
+++ src/my_project/matrix/ThreeDiagonalMatrix.cpp
@@ -1,1 +1,54 @@
+//
+// Created by vladimir on 05.02.2022.
+//
 
+#include "ThreeDiagonalMatrix.hpp"
+
+namespace Slae::Matrix {
+
+    ThreeDiagonalMatrix::ThreeDiagonalMatrix(int size) : data_(size) {}
+
+    ThreeDiagonalMatrix ThreeDiagonalMatrix::Zero(int size) {
+        ThreeDiagonalMatrix result(size);
+        for (auto& string : result.data_) {
+            string = {0., 0., 0.};
+        }
+        return result;
+    }
+
+    double &ThreeDiagonalMatrix::operator()(int i, int j) {
+#ifndef NDEBUG
+        if (i >= data_.size()) {
+            std::stringstream buff;
+            buff << "Индекс i превышает размер матрицы! Полученный индекс: " << i << ". Размер матрицы: " << data_.size() << ". Файл: " << __FILE__ << ". Строка: " << __LINE__;
+            throw SlaeBaseExceptionCpp(buff.str());
+        }
+        if (j > 2) {
+            std::stringstream buff;
+            buff << "Индекс j должен пинадлежать {0, 1, 2}! Полученный индекс: " << j << ". Размер матрицы: " << data_.size() << ". Файл: " << __FILE__ << ". Строка: " << __LINE__;
+            throw SlaeBaseExceptionCpp(buff.str());
+        }
+#endif NDEBUG
+        return data_[i][j];
+    }
+
+    const double &ThreeDiagonalMatrix::operator()(int i, int j) const{
+#ifndef NDEBUG
+        if (i >= data_.size()) {
+            std::stringstream buff;
+            buff << "Индекс i превышает размер матрицы! Полученный индекс: " << i << ". Размер матрицы: " << data_.size() << ". Файл: " << __FILE__ << ". Строка: " << __LINE__;
+            throw SlaeBaseExceptionCpp(buff.str());
+        }
+        if (j > 2) {
+            std::stringstream buff;
+            buff << "Индекс j должен пинадлежать {0, 1, 2}! Полученный индекс: " << j << ". Размер матрицы: " << data_.size() << ". Файл: " << __FILE__ << ". Строка: " << __LINE__;
+            throw SlaeBaseExceptionCpp(buff.str());
+        }
+#endif NDEBUG
+        return data_[i][j];
+    }
+
+    unsigned int ThreeDiagonalMatrix::rows() const noexcept {
+        return (data_.size());
+    }
+}
